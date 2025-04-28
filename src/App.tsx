7de59e8dfc9acc5ae823c716a583c264f08479a6
john.gong <john.gong@leapdev.io>
2025-04-28 15:33:18 +1000

    fix: update deployspec

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <>
-            <h1>Yara Johns</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-            <p className="read-the-docs">Welcome to my website</p>
-        </>
-    );
+    return <div>
+        
+    </div>;
 }
 
 export default App;
